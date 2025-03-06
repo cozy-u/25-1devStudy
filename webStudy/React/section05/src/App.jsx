d62896cb272dd72bee8985dae2255f4d75578398
cozy-u <164289103+cozy-u@users.noreply.github.com>
2025-03-07 01:09:44 +0900

    250306

--- webStudy/React/section05/src/App.jsx
+++ webStudy/React/section05/src/App.jsx
@@ -1,31 +1,11 @@
 import './App.css'
-import Header from "./components/Header.jsx"
-import Main from "./components/Main.jsx"
-import Footer from "./components/Footer.jsx"
-import Button from "./components/Button.jsx"
-
-  function App() { //부모 컴포넌트 
-  const buttonProps = {
-    text:"메일",
-    color:"red",
-    a:1,
-    b:2,
-    c:3,
-  }
-
+import Register from "./components/Register";
+  function App() {     
   return (
     <>
-    
-    <Main />
-    <Button {...buttonProps}/>
-    <Button text={"카페"}/>
-    <Button text={"블로그"}>
-      <Header />   
-    </Button>
-    <Footer />
-
+    <Register />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
